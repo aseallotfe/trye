87b7ed8bed19bb0699c58b8bbb76b3f763012587
aseallotfe <133778455+aseallotfe@users.noreply.github.com>
2023-05-21 19:18:01 +0300

    Add files via upload

---
+++ ConsoleApp4/Employee.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    internal class Employee
+    {
+        private string name;
+        private double salary;
+        private string address;
+        public Employee()
+        {
+            Console.WriteLine("Welcome to our company");
+        }
+        public Employee(string name,double salary,string address):this()
+        {
+            this.name = name;
+            Console.WriteLine(name);
+            this.salary = salary;
+            Console.WriteLine(salary);
+            this.address = address;
+            Console.WriteLine(address);
+        }
+    }
+}
---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConsoleApp4;
+
+Employee sd = new Employee();
+Employee sd1 = new Employee("Unknown", 30000, "Mukalla");
